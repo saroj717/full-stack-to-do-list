5977d66d7f65d36f4f29c7083129186868b21016
h8rry <harrychenca@gmail.com>
2024-05-15 12:45:08 -0700

    Fixes

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -2,18 +2,39 @@ import $ from "jquery";
 
 import { indexTasks, postTask } from "./requests.js";
 
-indexTasks(function (response) {
-  var htmlString = response.tasks.map(function (task) {
-    return (
-      "<div class='col-12 mb-3 p-2 border rounded task' data-id='" +
-      task.id +
-      "'> \
-      " +
-      task.content +
-      "\
-      </div>"
-    );
+function getTasks() {
+  indexTasks(function (response) {
+    var htmlString = response.tasks.map(function (task) {
+      return (
+        "<div class='col-12 mb-3 p-2 border rounded task' data-id='" +
+        task.id +
+        "'> \
+        " +
+        task.content +
+        "\
+        </div>"
+      );
+    });
+  
+    $("#tasks").html(htmlString);
   });
+}
 
-  $("#tasks").html(htmlString);
+function createTask(value) {
+  console.log(value);
+
+  // use postTask()
+}
+
+// document ready
+$(function () {
+  console.log("document ready");
+  getTasks();
+
+  $("#task-form").on("submit", function (event) {
+    event.preventDefault();
+    
+    const value = $("#task-input").val();
+    createTask(value);
+  });
 });
